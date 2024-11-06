b24aad83a1f418796b2b4827fcc673c15bae3aa8
welinton-cool <welinton.sacon@espoch.edu.ec>
2024-11-06 15:26:00 -0500

    hola

---
+++ src/main/java/ec/edu/ec/tic/TIC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.tic;
+
+/**
+ *
+ * @author HP
+ */
+public class TIC {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA");
+    }
+}
